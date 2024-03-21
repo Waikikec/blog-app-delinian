86612b51b8960d88ce8f6188c8c17b1c5f4c7264
MartinYankov <martin.d.yankov@gmail.com>
2024-03-21 13:07:54 +0200

    feat: add first toolbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted value:", inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/">Global Capital</a>
+        <a href="/">Securitization</a>
+        <a href="/">Global Markets</a>
+      </div>
+      <div className="right">
+        {isSearchActive ? (
+          <form onSubmit={handleSubmit} className="searchWrapper">
+            <div className="searchLeft">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="magnifyIcon"
+              />
+              <input
+                className="searchInput"
+                placeholder="Search key words, topics"
+                onChange={handleChange}
+                value={inputValue}
+              />
+            </div>
+            <FontAwesomeIcon
+              className="xMarkIcon"
+              icon={faXmark}
+              onClick={() => setIsSearchActive(false)}
+            />
+          </form>
+        ) : (
+          <button
+            href="/Search"
+            className="searchButton"
+            onClick={() => setIsSearchActive(true)}
+          >
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        )}
+
+        <a href="/Pricing" className="pricing">
+          Pricing
+        </a>
+        <a href="/Plan" className="plan">
+          Free Trial
+        </a>
+        <a href="/Login" className="login">
+          Login
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
